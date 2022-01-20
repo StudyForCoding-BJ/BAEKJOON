27e61c20a1fc10bd0294e21f18b2ae9673fe4d49
Sumin Chung <87513424+finesum@users.noreply.github.com>
2022-01-20 12:21:10 +0900

    Update 9020 to better version

--- 09_Math2/Step06/9020_better.py
+++ 09_Math2/Step06/9020_better.py
@@ -9,27 +9,24 @@ def era(start, end: int):
             for j in range(i*2, end+1, i):
                 era[j] = 0
 
-    primelist = []
-    for i in range(start, end+1):
-        if era[i] == 1:
-            primelist.append(i)
-    return primelist
+    return era
 
 for i in range(int(input())):
     even = int(input())
-    total = era(2, even)
-    for j in range(int(even/2), 1, -1):
-        if j in total:
-            if even-j in total:
-                print (j, even-j)
-                break
+    total = era(1, even)
+    a = even//2
+    b = a
+    for j in range(even):
+        if total[a]==1 and total[b] == 1:
+            print(a,b)
+            break
+        a -= 1
+        b += 1
 
 """
 에라토스테네스의 체 시간복잡도: O(NloglogN)
 O(NloglogN)은 O(1)에 가까울정도로 단순
-
-범위 제한이 있을 경우엔 범위에 맞게 리스트를 먼저 구한 뒤에 처리하는게 빠름
-
+범위 제한이 있을 경우엔 위의 풀이처럼 범위에 맞게 리스트를 먼저 구한 뒤에 처리하는게 빠름
 참고: https://continuous-development.tistory.com/204
 """
-            
+          
