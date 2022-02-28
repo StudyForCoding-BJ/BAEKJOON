6fd6ac4ec91995c109a141191010ff0b67288dff
Sumin Chung <sm990223@gmail.com>
2022-02-28 15:25:46 +0900

    Update 5430.py

--- 18_Queue, Dequeue/Step07/5430.py
+++ 18_Queue, Dequeue/Step07/5430.py
@@ -1,17 +1,17 @@
 #BOJ 5430
-#cmd.replace가 여기서 대체 왜 안 되는지 모르겠음
+#136ms, print로 쓰면 136ms
 import sys
 tc = int(input())
 
 def sol(cmd, n, x):
-    cmd.replace("RR", "")
+    cmd = cmd.replace("RR", "")
     l = 0
     r = 0
     rev = True
     
     for i in cmd:
         if i == 'R':
-            rev = False
+            rev = not rev
         else:
             if rev == True:
                 l += 1
@@ -20,15 +20,15 @@ def sol(cmd, n, x):
     if l+r <= n:
         res = x[l:n-r]
         if rev == True:
-            return '['+','.join(res)+']\n'
+            return '['+','.join(res)+']'
         else:
-            return '['+','.join(res[::-1])+']\n'
+            return '['+','.join(res[::-1])+']'
     else:
-        return 'error\n'
+        return 'error'
 
 for i in range(tc):
     cmd = sys.stdin.readline().rstrip()
     n = int(sys.stdin.readline().strip())
     x = sys.stdin.readline().rstrip()[1:-1].split(',')
     if n == 0: []
-    sys.stdout.write(sol(cmd, n, x))
+    print(sol(cmd, n, x))
